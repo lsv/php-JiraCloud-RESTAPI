694c9d4f437a6fa2d2e5580e22abecf59aa900ef
Martin Aarhof <martin.aarhof@gmail.com>
2023-11-22 00:11:57 +0100

    User Deprecated: Method "JsonSerializable::jsonSerialize()" might add "mixed" as a native return type declaration in the future. Do the same in implementation "JiraCloud\Issue\IssueType"

--- src/Issue/IssueType.php
+++ src/Issue/IssueType.php
@@ -24,7 +24,7 @@ class IssueType implements \JsonSerializable
     public int $avatarId;
 
     #[\ReturnTypeWillChange]
-    public function jsonSerialize()
+    public function jsonSerialize(): array
     {
         return array_filter(get_object_vars($this));
     }
